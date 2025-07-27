4d43ea4443703476aed827e5a30e984f45cbadab
nguyendanhminhtan <lykien85@gmail.com>
2025-07-27 16:44:16 +0700

    lam bai 3.py

--- bai1/luyện tập/bài 1.py
+++ bai1/luyện tập/bài 1.py
@@ -1,1 +1,4 @@
 print("Kết quả phép tính 10 + 13 là: ", 10+13)
+print("Kết quả phép tính 20-7 là:", 20-7)
+print("Kết quả của phép tính 3*10-16 là:",3*10-16)
+print("Kết quả của phép tính 12/5+13/6 là:",12/5+13/6)
---
+++ bai1/luyện tập/lam bai 3.py
@@ -0,0 +1,2 @@
+print("Kết quả cảu phép tính 1*3*5*7 =",1*3*5*7)
+print("Bạn Hoa năm nay 16 tuổi")
