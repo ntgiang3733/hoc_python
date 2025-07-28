54fc816ad901b6a7ccff936586bb233be25aa9f2
nguyendanhminhtan <lykien85@gmail.com>
2025-07-28 19:59:50 +0700

    nhiem vu 1.py

---
+++ bai 17/thuc hanh/nhiem vu 1.py
@@ -0,0 +1,9 @@
+print("Giá trị của biểu thức (1+2+3+4+5+6+7+8+9+10)**3 là:",(1+2+3+4+5+6+7+8+9+10)**3)
+x=2
+y=5
+print("Giá trị của biểu thức (x+y)*(x**2+y**2-1) là:",(x+y)*(x**2+y**2-1))                           
+print("Kết quả của phép tính 1/2+1/3+1/4+1/5 là:",1/2+1/3+1/4+1/5)
+a=2
+b=3
+c=4
+print("Giad trị của biểu thức (a+b+c)*(a+b-c) là:",(a+b+c)*(a+b-c)) 
