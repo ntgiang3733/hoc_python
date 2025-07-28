64d7ca6ae0e2e58b0bc171e754870f42539f4e7c
ntgiang <ntgiang3733@gmail.com>
2025-07-28 19:41:10 +0700

    baif17-thực hàng 1c

--- bai1/bien.py
+++ bai1/bien.py
@@ -1,5 +1,11 @@
-x,y = 5,2
-print("Kết quả phép cộng x + y là: ", x+y)
-print("Kết quả phép cộng x - y là: ", x-y)
-print("Kết quả phép cộng x * y là: ", x*y)
-print("Kết quả phép cộng x / y là: ", x/y)
+# x,y = 5,2
+# print("Kết quả phép cộng x + y là: ", x+y)
+# print("Kết quả phép cộng x - y là: ", x-y)
+# print("Kết quả phép cộng x * y là: ", x*y)
+# print("Kết quả phép cộng x / y là: ", x/y)
+
+# Bài thực hành
+# c) Thực hiện gán x=2, y=5 rồi tính giá trị biểu thức (x+y)(x**2 + y**2 - 1)
+x=2
+y=5
+print("Giá trị của biểu thức (x+y)(x**2+y**2-1) là:", (x+y)*(x**2+y**2-1))
